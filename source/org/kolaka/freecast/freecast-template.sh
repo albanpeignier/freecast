c4a97f7c74237049e15938330e5cb5e2ac6f3dcf
alban <alban@20e14386-5fff-0310-853f-b84fc43df78c>
2006-02-25 14:52:44 +0000

    [Bug 67] modified java version checking to support kaffe jvm
    git-svn-id: http://svn.tryphon.org/kolaka/freecast/trunk@252 20e14386-5fff-0310-853f-b84fc43df78c

--- source/org/kolaka/freecast/freecast-template.sh
+++ source/org/kolaka/freecast/freecast-template.sh
@@ -68,10 +68,8 @@ done
 JAVA_CMD=java
 [ -n "$JAVA_HOME" ] && JAVA_CMD=$JAVA_HOME/bin/java
 
-$JAVA_CMD -version 2>&1 | egrep 'java version "1.[45]' > /dev/null
-error_level=$?
-
-if [ $error_level != 0 ]; then
+$JAVA_CMD -version 2>&1 | egrep '(java version "1.[45]|java full version "kaffe-1.[45])' > /dev/null
+if [ $? != 0 ]; then
     echo "A jdk 1.4 is needed to run @app.name@. Check your JAVA_HOME variable." >&2
     exit 1
 fi
