7809130f42f5b6004738f9f619def6f9255f5af4
alban <alban@20e14386-5fff-0310-853f-b84fc43df78c>
2006-04-15 14:28:46 +0000

    fixed nextMustBeFirstPage reset when cached header is used
    git-svn-id: http://svn.tryphon.org/kolaka/freecast/trunk@436 20e14386-5fff-0310-853f-b84fc43df78c

--- source/org/kolaka/freecast/pipe/DefaultPipe.java
+++ source/org/kolaka/freecast/pipe/DefaultPipe.java
@@ -162,6 +162,7 @@ public class DefaultPipe implements Pipe {
 
 				LogicalPage returnedPage = headerPage;
 				headerPage = null;
+				nextMustBeFirstPage = false;
 				return returnedPage;
 			}
 
