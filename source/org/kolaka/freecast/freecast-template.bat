5932e7bab14a8132a4d650d6bf2922313d89bef0
alban <alban@20e14386-5fff-0310-853f-b84fc43df78c>
2006-04-24 20:47:26 +0000

    [Bug 72] add a pause when the java exits with error, set PATH to include native libraries
    git-svn-id: http://svn.tryphon.org/kolaka/freecast/trunk@465 20e14386-5fff-0310-853f-b84fc43df78c

--- source/org/kolaka/freecast/freecast-template.bat
+++ source/org/kolaka/freecast/freecast-template.bat
@@ -39,6 +39,10 @@ if exist %FREECAST_HOME%\etc set CLASSPATH=%FREECAST_HOME%\etc
 FOR  %%i IN (%LIBDIR%\*.jar) DO call %FREECAST_HOME%\bin\classpath %%i
 FOR  %%i IN (%LIBDIR%\windows\*.jar) DO call %FREECAST_HOME%\bin\classpath %%i
 
-%JAVA_CMD% -classpath %CLASSPATH% -Djava.library.path=%LIBDIR%\windows\x86 -Dapp.name=@app.name@ -Dlog.dir=%LOGDIR% -Dlib.dir=%LIBDIR% @app.mainclass@ %*
+set PATH=%PATH%;%LIBDIR%\windows\x86
+
+%JAVA_CMD% -classpath %CLASSPATH% -Dapp.name=@app.name@ -Dlog.dir=%LOGDIR% -Dlib.dir=%LIBDIR% @app.mainclass@ %*
+
+if ERRORLEVEL 1 pause
 
 :end
